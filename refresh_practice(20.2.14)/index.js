8724ed3fd603e383f6318ec1291dff6800d39154
mandoremee <gpffna777@naver.com>
2020-02-17 17:39:03 +0900

    What is DOM ?

--- refresh_practice(20.2.14)/index.js
+++ refresh_practice(20.2.14)/index.js
@@ -1,7 +1,11 @@
 //console.log(document.getElementById);
 
+//DOM 자바스크립트로 HTML을 바꿀 수 있다!
 const title = document.getElementById("title");
 title.innerHTML = "GREAT!";
 console.log(title);
-
+console.dir(title);
+console.dir(document);
+document.title = ("i own you now!")
+title.style.color = "red";
 console.error("NOOOOOOOOOOOO");
